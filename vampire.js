89d4429ee4ae56000d3eef0d10bb609d612b23d7
J8051 <nathaniel.jessica@yahoo.ca>
2023-02-17 16:13:36 -0500

    vampire exercise part 2

--- vampire.js
+++ vampire.js
@@ -10,13 +10,13 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-    this.offspring.push(vampire); 
+    this.offspring.push(vampire);
     vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-    return this.offspring.length; 
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
@@ -32,24 +32,47 @@ class Vampire {
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-    return (vampire.numberOfVampiresFromOriginal > this.numberOfVampiresFromOriginal)
+    return (vampire.numberOfVampiresFromOriginal > this.numberOfVampiresFromOriginal);
   }
 
   /** Tree traversal methods **/
 
   // Returns the vampire object with that name, or null if no vampire exists with that name
   vampireWithName(name) {
-    
-  }
+    if (this.name === name) {
+      return this;
+    }
+    for (const child of this.offspring) {
+      const found = child.vampireWithName(name);
+      if (found) {
+        return found;
+      }
+    }
+    return null;
+  };
 
   // Returns the total number of vampires that exist
   get totalDescendents() {
-    
+    let numberOfVamps = this.offspring.length;
+    for (let child of this.offspring) {
+      numberOfVamps += child.totalDescendents;
+    }
+    return numberOfVamps;
   }
 
   // Returns an array of all the vampires that were converted after 1980
   get allMillennialVampires() {
-    
+    console.log(this.name, this.yearConverted); 
+    let millenials = [];
+    if (this.yearConverted > 1980) {
+      millenials.push(this);
+    }
+
+    for (let child of this.offspring) {
+      const answer = child.allMillennialVampires;
+      millenials = millenials.concat(answer); 
+    }
+    return millenials;
   }
 
   /** Stretch **/
